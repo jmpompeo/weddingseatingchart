2b0b3fb725fd6ae4b880c19732eba9cd7d799e39
jmpompeo <75942346+jmpompeo@users.noreply.github.com>
2022-09-12 12:28:08 -0400

    Repo setup (#1)
    * setup repo/solution
    
    * added http client & log to function

---
+++ GuestListTable/GuestListTable.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace GuestListTable
+{
+    public class GuestListTable
+    {
+        private readonly ILogger _log;
+        private readonly HttpClient client;
+        public GuestListTable(ILogger log, IHttpClientFactory httpClientFactory)
+        {
+            _log = log;
+            client = httpClientFactory.CreateClient();
+        }
+
+        [FunctionName("GuestListTable")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name ??= data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
